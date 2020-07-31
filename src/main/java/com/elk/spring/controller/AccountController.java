8a2a009c8c63bd086276fd61c6596ecbb9e80fd6
harshgajjar7110 <30972844+harshgajjar7110@users.noreply.github.com>
2020-07-31 17:37:05 +0530

    added more query

--- src/main/java/com/elk/spring/controller/AccountController.java
+++ src/main/java/com/elk/spring/controller/AccountController.java
@@ -2,12 +2,8 @@ package com.elk.spring.controller;
 
 import java.util.List;
 
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.elk.spring.pojo.Accounts;
 import com.elk.spring.repository.AccountRepository;
 import com.elk.spring.service.AccountService;
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
-import lombok.extern.log4j.Log4j2;
 
 @RestController
-@Log4j2
+
 public class AccountController {
 	@Autowired
 	ElasticsearchOperations esTemplate;
@@ -44,6 +38,24 @@ public class AccountController {
 	@GetMapping("/findByName/{name}")
 	public List<Accounts> saveCustomer(@PathVariable String name) throws Exception {
 		return service.findByFirstNameLastName(name);
+
+	}
+
+	@GetMapping("/find/{field}/{name}")
+	public List<Accounts> wildCardQuery(@PathVariable String field, @PathVariable String name) throws Exception {
+		return service.wildCardQuery(field, name);
+
+	}
+
+	@GetMapping("/findDisQuery/{name}")
+	public List<Accounts> disMaxQuery(@PathVariable String name) throws Exception {
+		return service.disMaxQuery(name);
+
+	}
+
+	@GetMapping("/filterByBalance/{from}/{to}")
+	public List<Accounts> filterByBalance(@PathVariable double from, @PathVariable double to) throws Exception {
+		return service.getResultByFilter(from, to);
 
 	}
 }
--- src/main/java/com/elk/spring/service/AccountService.java
+++ src/main/java/com/elk/spring/service/AccountService.java
@@ -2,11 +2,13 @@ package com.elk.spring.service;
 
 import java.util.List;
 
-import org.apache.lucene.util.QueryBuilder;
+import org.apache.lucene.search.TermQuery;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.DisMaxQueryBuilder;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.WildcardQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
@@ -14,8 +16,10 @@ import org.springframework.stereotype.Service;
 
 import com.elk.spring.pojo.Accounts;
 import com.elk.spring.repository.AccountRepository;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
 @Service
+
 public class AccountService {
 
 	@Autowired
@@ -30,20 +34,46 @@ public class AccountService {
 
 	public List<Accounts> findByFirstName(String name) {
 		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
-				.must(QueryBuilders.matchQuery("firstname", name).fuzziness("AUTO"));
+				.must(QueryBuilders.matchQuery("firstname", name).fuzziness(Fuzziness.AUTO));
 
-		NativeSearchQuery nativeSearchQuery = new NativeSearchQuery(queryBuilder);
-		List<Accounts> users = template.queryForList(nativeSearchQuery, Accounts.class);
-		return users;
+		return generateResult(queryBuilder);
 
 	}
 
 	public List<Accounts> findByFirstNameLastName(String name) {
-
+		// find query on multiple fields
 		MultiMatchQueryBuilder builder = QueryBuilders.multiMatchQuery(name, "firstname", "lastname")
 				.fuzziness(Fuzziness.AUTO);
+
+		return generateResult(builder);
+	}
+
+	public List<Accounts> wildCardQuery(String field, String name) {
+		// wildcard query
+		WildcardQueryBuilder wildcardQuery = QueryBuilders.wildcardQuery(field, name + "*");
+		return generateResult(wildcardQuery);
+	}
+
+	public List<Accounts> getResultByFilter(double from, double to) {
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
+
+				.filter(QueryBuilders.rangeQuery("balance").gt(from).lt(to));
+
+		return generateResult(queryBuilder);
+
+	}
+
+	public List<Accounts> disMaxQuery(String name) {
+		// condition 1 or condition 2
+		DisMaxQueryBuilder disMaxQueryBuilder = QueryBuilders.disMaxQuery().add(termQuery("firstname", name))
+				.add(termQuery("lastname", name));
+
+		return generateResult(disMaxQueryBuilder);
+	}
+
+	private List<Accounts> generateResult(org.elasticsearch.index.query.QueryBuilder builder) {
 		NativeSearchQuery nativeSearchQuery = new NativeSearchQuery(builder);
-		List<Accounts> users = template.queryForList(nativeSearchQuery, Accounts.class);
-		return users;
+
+		return template.queryForList(nativeSearchQuery, Accounts.class);
 	}
 }
